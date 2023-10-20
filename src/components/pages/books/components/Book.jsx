68ac31af1f82e00492ee18ee432961196a732418
sabirama <markcondes21@gmail.com>
2023-10-20 19:41:00 +0800

    test deploy 3

---
+++ src/components/pages/books/components/Book.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+import { Grid } from "semantic-ui-react";
+
+const Book = (prop) => {
+  return (
+    <>
+      <Grid.Column className="container book">
+        <Link to={prop.book.title} className="container book-link width-90 height-90">
+          <img src={prop.book_image} alt="book image" className="book-image" />
+          <h1>{prop.book.title}</h1>
+          <p>for: {prop.book.price} Php</p>
+        </Link>
+      </Grid.Column>
+    </>
+  );
+};
+
+export default Book;
