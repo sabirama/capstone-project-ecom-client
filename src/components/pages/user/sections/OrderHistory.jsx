0e382e9d71f1295f5c2f73b05ae397c97f37da34
sabirama <markcondes21@gmail.com>
2023-10-22 04:34:32 +0800

    removed unused imports from order history

--- src/components/pages/user/sections/OrderHistory.jsx
+++ src/components/pages/user/sections/OrderHistory.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { Segment } from "semantic-ui-react";
 import Get from "../../../lib/http/get";
 import OrderDetails from "./components/OrderDetails";
 
